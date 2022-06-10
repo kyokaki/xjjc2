5a278963f6837f656c286394f96714f38733f0c7
sg <sg@sg.com>
2022-06-10 18:05:56 +0800

    feat(page): modify

--- src/utils/request.js
+++ src/utils/request.js
@@ -18,12 +18,6 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['User-Token'] = getToken()
-    }
     if (config.data) {
       config.data.requestSystem = 'compute-h5'
       config.data.traceLogId = uuid()
